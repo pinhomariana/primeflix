03622e52f937ff4392dd2759a2a69a85adefe91f
pinhomariana <marianapinho1109@gmail.com>
2024-08-14 13:20:06 +0100

    updated routes

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Movies from "./Pages/Movies";
 import Header from "./Components/Header";
-import React from 'react'
+import React from 'react';
 import Error from "./Pages/Error";
 import Favourites from "./Pages/Favourites";
 
 export default function RoutesApp() {
   return (
-    <BrowserRouter>
-    <Header/>
-        <Routes>
-            <Route path="/" element={ <Home/> }/>
-            <Route path="/movie/:id" element={ <Movies/> }/>
-            <Route path="/favourites" element={ <Favourites/> }/>
-
-            <Route path="*" element={ <Error/>}/>
-        </Routes>
-    </BrowserRouter>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Movies />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
 }
